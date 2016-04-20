c882a006dfb5f1faef1a159d27b950e316ae7fc4
guhun_gao <ggjgao@gmail.com>
2016-04-20 19:33:58 +0800

    第一次提交github

---
+++ src/pers/guhun/git/GitTest.java
@@ -0,0 +1,22 @@
+/**
+ * @file: GitTest.java
+ * @package: pers.guhun.git
+ * @Description: TODO 
+ * @author: gao   
+ * @date: 2016年4月20日 下午7:25:19 
+ * @version V1.0
+ */
+package pers.guhun.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description: TODO
+ * @author: gao
+ * @date: 2016年4月20日 下午7:25:19
+ * @email: 1534759775@qq.com
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("this is my first program in github");
+	}
+}
